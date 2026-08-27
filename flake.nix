cfcc6f6769e486d89d801ae52260d18ea7428e0e
keke <zrignshw@duck.com>
2026-08-27 20:07:45 +0800

    fix build

--- flake.nix
+++ flake.nix
@@ -1,22 +1,34 @@
 {
   description = "Kiki — a tiny e-ink Android launcher";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      buildTools = pkgs.fetchurl {
-        url = "https://dl.google.com/android/repository/build-tools_r34-linux.zip";
-        sha256 = "e858c4b60069d0431051b225d384413b1643e1289b00a4825aed347f25bd510f";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          android_sdk.accept_license = true;
+        };
       };
-      platform = pkgs.fetchurl {
-        url = "https://dl.google.com/android/repository/platform-34-ext7_r03.zip";
-        sha256 = "16fdb74c55e59ae3ef52def135aec713508467bd56d7dabcd8c9be31fa8b20f3";
+
+      android = pkgs.androidenv.composeAndroidPackages {
+        buildToolsVersions = [ "34.0.0" ];
+        platformVersions = [ "34" ];
+        includeEmulator = false;
+        includeNDK = false;
+        includeCmake = false;
+        includeSources = false;
+        includeSystemImages = false;
       };
-    in {
-      packages.${system}.kiki = pkgs.stdenvNoCC.mkDerivation {
+
+      sdk = "${android.androidsdk}/libexec/android-sdk";
+      buildTools = "${sdk}/build-tools/34.0.0";
+      androidJar = "${sdk}/platforms/android-34/android.jar";
+
+      kiki = pkgs.stdenvNoCC.mkDerivation {
         pname = "kiki";
         version = "1.4";
         src = pkgs.lib.cleanSourceWith {
@@ -29,18 +41,14 @@
             && !(pkgs.lib.hasSuffix ".apk" b)
             && !(pkgs.lib.hasSuffix ".idsig" b);
         };
-        nativeBuildInputs = [ pkgs.jdk17 pkgs.unzip pkgs.python3 ];
+        nativeBuildInputs = [ pkgs.jdk17 pkgs.python3 ];
         dontConfigure = true;
         dontPatchELF = true;
         dontStrip = true;
         buildPhase = ''
           runHook preBuild
-          sdk=$(mktemp -d)
-          unzip -q ${buildTools} -d "$sdk"
-          unzip -q ${platform} -d "$sdk"
-          export ANDROID_SDK="$sdk"
-          export ANDROID_BUILD_TOOLS="$sdk/android-14"
-          export ANDROID_JAR="$sdk/android-34/android.jar"
+          export ANDROID_BUILD_TOOLS="${buildTools}"
+          export ANDROID_JAR="${androidJar}"
           bash ./build.sh "$PWD/out"
           runHook postBuild
         '';
@@ -54,10 +62,19 @@
           platforms = [ system ];
         };
       };
-      packages.${system}.default = self.packages.${system}.kiki;
-
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [ pkgs.jdk17 pkgs.python3 pkgs.unzip pkgs.zip ];
+    in {
+      packages.${system} = {
+        inherit kiki;
+        default = kiki;
+      };
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          packages = [ pkgs.jdk17 pkgs.python3 android.androidsdk ];
+          ANDROID_HOME = sdk;
+          ANDROID_SDK_ROOT = sdk;
+          ANDROID_BUILD_TOOLS = buildTools;
+          ANDROID_JAR = androidJar;
+        };
       };
     };
 }
